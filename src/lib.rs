97a10a87ba783c09e654b4b3d70c09a894a3c8f8
Justin S <JustinJSumner@gmail.com>
2019-02-20 08:32:00 -0800

    asd

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,27 @@
-pub struct ThreadPool;
+use std::sync::mpsc;
+use std::thread;
 
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
     pub fn new(size:usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id), receiver);
+        }
+
+        ThreadPool {
+            workers,
+            sender,
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -14,3 +31,22 @@ impl ThreadPool {
 
     }
 }
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, eceiver: mpsc::Receiver<Job>) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread,
+        }
+    }
+}
+
+struct Job;
+
